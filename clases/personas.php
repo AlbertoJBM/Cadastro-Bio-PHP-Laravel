41cd25527df09b236cefbedb9757634ee308c85d
AlbertoJBM <albertobejarano2@gmail.com>
2023-02-24 15:33:18 -0300

    agregar la clase persona

---
+++ clases/personas.php
@@ -0,0 +1,14 @@
+<?php
+
+class persona{
+    public $cpf= 123;//->primary();//intero
+            //---------------------------
+    public $foto="url ramdom";//modificar solo ejemplo
+            //------------------------
+    public $nome="pepito";//varchar 220
+    public $idade=12;
+    public $resumo="soy el mejor pepito";
+    public $profesao="peitero";
+}
+
+?>
